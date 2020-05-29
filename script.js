717f0089e02a3a13b32266ca5d0af01875315d8b
Himalaya <yugmani@yahoo.com>
2020-05-28 22:59:14 -0700

    parseInt and parseFloat compared

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log(parseInt("14")); 
+console.log(parseInt("cool567"));
+console.log(parseInt("+34blah"));
+console.log(parseInt("-23awesome"));
+console.log(parseInt("020"));
+console.log(parseInt("0xA"));
+
+console.log(parseFloat("14")); 
+console.log(parseFloat("blah22"));
+console.log(parseFloat("123.456"));
+console.log(parseFloat("123.45.6"));
+console.log(parseFloat("023.44"));
+console.log(parseFloat("0xAF"));
+
+console.log(parseInt("11", 10)); 
+console.log(parseInt("11", 2));
+console.log(parseInt("11", 16));
+console.log(parseInt("11", 8));
